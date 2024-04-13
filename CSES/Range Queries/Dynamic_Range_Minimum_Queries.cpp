7563083db5c0ceb940b68b264ea9fd952a697124
AbhijeetJ <01fe21bei045@kletech.ac.in>
2024-04-13 23:47:26 +0530

    Implemented Dynamic Range Minimum Queries algorithm in C++
    The Dynamic Range Minimum Queries algorithm targets the efficient handling of range minimum queries within arrays.

---
+++ CSES/Range Queries/Dynamic_Range_Minimum_Queries.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <stdlib.h>
+#include <limits.h>
+
+using namespace std;
+
+int min(int a, int b)
+{
+    return (a < b) ? a : b;
+}
+void build_tree(int arr[], int tree[], int node, int start, int end)
+{
+    if (start == end)
+    {
+        tree[node] = arr[start];
+    }
+    else
+    {
+        int mid = start + (end - start) / 2;
+        build_tree(arr, tree, 2 * node, start, mid);
+        build_tree(arr, tree, 2 * node + 1, mid + 1, end);
+        tree[node] = min(tree[2 * node], tree[2 * node + 1]);
+    }
+}
+
+int query(int tree[], int node, int start, int end, int left, int right)
+{
+    if (right < start || left > end)
+    {
+        return INT_MAX;
+    }
+    if (left <= start && right >= end)
+    {
+        return tree[node];
+    }
+    int mid = (start + end) / 2;
+    return min(query(tree, 2 * node, start, mid, left, right),
+               query(tree, 2 * node + 1, mid + 1, end, left, right));
+}
+
+void update(int tree[], int node, int start, int end, int idx, int value)
+{
+    if (start == end)
+    {
+        tree[node] = value;
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        if (idx <= mid)
+        {
+            update(tree, 2 * node, start, mid, idx, value);
+        }
+        else
+        {
+            update(tree, 2 * node + 1, mid + 1, end, idx, value);
+        }
+        tree[node] = min(tree[2 * node], tree[2 * node + 1]);
+    }
+}
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int tree[2 * n];
+    for (int i = 0; i < 2 * n; i++)
+    {
+        tree[i] = INT_MAX;
+    }
+
+    build_tree(arr, tree, 1, 0, n - 1);
+
+    while (q--)
+    {
+        int query_type, a, b;
+        cin >> query_type >> a >> b;
+
+        if (query_type == 2)
+        {
+            cout << query(tree, 1, 0, n - 1, a - 1, b - 1) << endl;
+        }
+        else if (query_type == 1)
+        {
+            update(tree, 1, 0, n - 1, a - 1, b);
+        }
+    }
+
+    return 0;
+}
